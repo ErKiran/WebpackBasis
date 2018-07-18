9bbe60fe418b9dfd75b633c08af2d1bbdf9a09d5
Kiran <kiruu1238@gmail.com>
2018-07-18 14:57:54 +0545

    CSS loader initalized for HRM

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = ()=> {
 var element = document.createElement('h1');
-element.innerHTML = 'Hello Webpack!!';
+element.innerHTML = 'Hello Webpack!! Adding React So SOON';
 return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,19 @@ app: PATHS.app
 output: {
 path: PATHS.build,
 filename: 'bundle.js'
+},
+module: {
+rules: [
+{
+// Test expects a RegExp! Note the slashes!
+test: /\.css$/,
+loaders: ['style-loader', 'css-loader'],
+// Include accepts either a path or an array of paths.
+include: PATHS.app
 }
+]
+}
+
 };
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
